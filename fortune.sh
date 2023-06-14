f50db906e35409360b54d696941b76890f0ed460
Cristobal Zatarain <cristobal.zatarain@gmail.com>
2023-06-14 02:56:35 +0000

    Save progress: 1430.1

--- fortune.sh
+++ fortune.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
+
 # Program to tell a persons fortune
+
 echo -e "\n~~ Fortune Teller ~~\n"
+
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-N=$(( $RANDOM % 6 ))
-GET_FORTUNE() {
-  # if not passed the first argument to the function
+N=$(( RANDOM % 6 ))
+
+function GET_FORTUNE() {
   if [[ ! $1 ]]
   then
-    echo "Ask a yes or no question:"
+    echo Ask a yes or no question:
   else
-    echo "Try again. Make sure it ends with a question mark:"
+    echo Try again. Make sure it ends with a question mark:
   fi
+
   read QUESTION
 }
-# Call the function First run without any argument
+
 GET_FORTUNE
-# The RegEx \?$ looks if a string last character is equal to '?'
+
 until [[ $QUESTION =~ \?$ ]]
 do
-  # Call the function for second time and beyond passing the argument 'again'
   GET_FORTUNE again
 done
-echo -e "\n${RESPONSES[$N]}"
+
+echo -e "\n$RESPONSES[$N]"
